14655702b30bb3b824017ba0b9e20bde2ceb68f4
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-12 22:13:57 +0500

    review all I've leraned

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orm_app.urls')),     # 👈 ROOT delegation
     path('blog/', include('my_app.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
